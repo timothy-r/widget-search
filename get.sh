eb74779b0ca728ff64ae525d98d2979391869ab5
Tim Rodger <tim.rodger@gmail.com>
2017-05-07 14:29:27 +0100

    More reorganisation

--- get.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-curl -XGET \
-   "https://search-trodger-e3ixwysobh5q7tqbo3qv4kopqu.eu-west-2.es.amazonaws.com/stock/widget/_search?q='urn:tjr:widget.135'"
--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+. ./.es-env
+
 # query using json from a file
 FILE=$1
 
 curl -XGET \
    -H "Content-type: application/json" \
-   'https://search-trodger-e3ixwysobh5q7tqbo3qv4kopqu.eu-west-2.es.amazonaws.com/stock/widget/_search?pretty' \
+   "$INDEX/_search?pretty" \
    -d @$FILE
